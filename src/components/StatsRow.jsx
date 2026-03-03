bfa02d05c3f1eecefe7ee8eaaae703e30ad541ef
Sandra Amurao <sandramae421@gmail.com>
2026-03-03 12:13:07 +0000

    styled statsrow

--- src/components/StatsRow.jsx
+++ src/components/StatsRow.jsx
@@ -1,23 +1,26 @@
+import "./StatsRow.css";
+import { BookMarked, Users, UserCheck } from "lucide-react";
+
 function StatsRow({ gitUser }) {
-    return (
-        <div className="flex flex-row max-[590px]:flex-wrap items-center justify-center gap-10">
-            <div className="w-full border p-8 flex flex-col items-center gap-y-4"> 
-                icon
-                <span> {gitUser.publicRepos} </span>
-                Repositories 
-            </div>
-            <div className="w-full border p-8 flex flex-col items-center gap-y-4"> 
-                icon
-                <span> {gitUser.followers} </span>
-                Followers 
-            </div>
-            <div className="w-full border p-8 flex flex-col items-center gap-y-4"> 
-                icon
-                <span> {gitUser.following} </span>
-                Following 
-            </div>
-        </div>
-    )
+	return (
+		<div className="stats-container">
+			<div className="stat-card border-[#dc4d15] shadow-[0_0_10px_#e98e43]">
+				<BookMarked size={25} color="#e98e43" />
+				<span className="text-lg"> {gitUser.publicRepos} </span>
+				<span className="text-lg">Repositories</span>
+			</div>
+			<div className="stat-card border-[#ffff03] shadow-[0_0_10px_#ecd862]">
+				<Users size={25} color="#ecd862"/> 
+				<span className="text-lg"> {gitUser.followers} </span>
+				<span className="text-lg">Followers</span>
+			</div>
+			<div className="stat-card border-[#7d09f2] shadow-[0_0_10px_#a360e7]">
+				<UserCheck size={25} color="#a360e7"/> 
+				<span className="text-lg"> {gitUser.following} </span>
+				<span className="text-lg"> Following </span>
+			</div>
+		</div>
+	);
 }
 
 export default StatsRow;
