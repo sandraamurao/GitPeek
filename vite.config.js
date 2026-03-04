fd77428d74bd5f43093d772edcac2e2aa1750c46
Sandra Amurao <sandramae421@gmail.com>
2026-03-04 11:18:42 +0000

    fix for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	base: "/git-peek/",
+	base: "/GitPeek/",
 	plugins: [react(), tailwindcss()],
 });
